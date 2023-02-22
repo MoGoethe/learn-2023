6f0af76263e2de60c7e07780106d88c9dfcdd81c
牟贵定 <seaseeyoul@mouguidingdeMac-Studio.local>
2023-02-22 22:20:48 +0800

    update

---
+++ nest-prisma-rest/products.ts
@@ -0,0 +1,62 @@
+export const products = [
+  {
+    name: 'Steven Jackson',
+    description: 'sifmthxslxajvndodegpgy',
+    price: 64.69,
+    sku: 'pho',
+  },
+  {
+    name: 'William Jones',
+    description: 'jodosrbqydetewabdbawjtbjcnnnunodfg',
+    price: 55.15,
+    sku: 'jvat',
+  },
+  {
+    name: 'Jeffrey Jackson',
+    description: 'uyfggijizrmhotbeatptjhsvtdgwf',
+    price: 11.34,
+    sku: 'ptcq',
+  },
+  {
+    name: 'Richard Taylor',
+    description: 'mqokvewtedkqwnbzudxzsgfmij',
+    price: 78.15,
+    sku: 'mrwc',
+  },
+  {
+    name: 'Michael Davis',
+    description: 'kurxtrphrjhpsrgyjzzbsphdu',
+    price: 53.62,
+    sku: 'ejm',
+  },
+  {
+    name: 'Brenda Robinson',
+    description: 'wxdlystyszpbjokrzlzyfxmcjhj',
+    price: 11.84,
+    sku: 'klcjv',
+  },
+  {
+    name: 'Elizabeth Gonzalez',
+    description: 'fsxqsuxppavxuldxyafgkreobidexiwggi',
+    price: 13.48,
+    sku: 'uquf',
+  },
+  {
+    name: 'Mary Young',
+    description: 'cwdhmohyksmzortxsjdkev',
+    price: 87.74,
+    sku: 'ljgu',
+  },
+  {
+    name: 'Barbara Jones',
+    description: 'qkqtwkhrjrsfqohffugkogkrrfqnubxc',
+    price: 69.22,
+    sku: 'imo',
+  },
+  {
+    name: 'Amy Young',
+    description: 'srgbowoimhjoqcpttwxwjhnqchoonzxtkewy',
+    price: 10.32,
+    sku: 'zod',
+  },
+];
